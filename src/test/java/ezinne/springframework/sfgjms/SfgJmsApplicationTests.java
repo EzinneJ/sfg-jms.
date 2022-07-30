add736abe8eb74086b05fa0de267af891df7f036
EzinneJ <chimejoyez@gmail.com>
2022-07-30 18:15:26 +0100

    JMS Messaging with Spring

---
+++ src/test/java/ezinne/springframework/sfgjms/SfgJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package ezinne.springframework.sfgjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
